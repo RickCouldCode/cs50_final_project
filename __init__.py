8718261ff1a9c605e2e029adee3ac86c94f32db4
Rick Zhou <rickyjoezhou@dhcp-10-250-162-235.harvard.edu>
2023-12-08 13:06:19 -0500

    Bug update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_wtf import CSRFProtect
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,4 +8,3 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'coolaf'
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
-migrate = Migrate(app, db)
